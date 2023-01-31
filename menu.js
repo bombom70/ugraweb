e025dbf7e5e9e51c99cb073a95002a017ff05c64
hamdamov-n <hamdamov-n@ugraweb.ru>
2023-01-31 12:35:16 +0500

    1.4. Added script for menu

--- menu.js
+++ menu.js
@@ -2,7 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const stateMenu = [
         {
             title: "Прямая трансляция",
-            children: []
+            children: [
+                {
+                    title: "О Ханты-Мансийске"
+                },
+                {
+                    title: "История кубка мира"
+                },
+                {
+                    title: "Информация для участников"
+                }
+            ]
         },
         {
             title: "Ханты-Мансийск",
@@ -40,7 +50,14 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         {
             title: "Контакты",
-            children: []
+            children: [
+                {
+                    title: "История кубка мира"
+                },
+                {
+                    title: "Информация для участников"
+                }
+            ]
         }
     ];
     const burgerMenu = document.querySelector(".burger-menu");
@@ -48,20 +65,25 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnBurgerMenu = document.querySelector(".btn__burger-menu");
     const btnСlose = document.querySelector(".btn-close");
     const headerMenu = document.querySelector(".header-menu");
-
     const generateMenu = (state, element, depth = 1) => {
         state.forEach(data => {
             const { title, children } = data;
             const menuEl = document.createElement("div");
-            const spanEl = document.createElement("span");
+            const aEl = document.createElement("a");
             menuEl.classList.add(`menu-item-${depth}`);
-            spanEl.classList.add(`menu-title-${depth}`);
-            spanEl.textContent = title;
-            menuEl.append(spanEl);
+            aEl.classList.add(`menu-title-${depth}`);
+            aEl.textContent = title;
+            menuEl.append(aEl);
             element.append(menuEl);
             if (children?.length > 0) {
-                spanEl.classList.add("arrow");
-                return generateMenu(children, menuEl, depth + 1);
+                const arrow = document.createElement("div");
+                arrow.classList.add("arrow");
+                console.log(children);
+                const subMenu = document.createElement("div");
+                subMenu.classList.add("sub-menu-mob");
+                aEl.append(arrow);
+                menuEl.append(subMenu);
+                return generateMenu(children, subMenu, depth + 1);
             }
         });
     };
@@ -83,4 +105,23 @@ document.addEventListener("DOMContentLoaded", () => {
         asd.classList.remove("isShow");
     });
 
+    const items = document.querySelectorAll(".menu-item-1");
+    [...items].forEach(item => {
+        item.addEventListener("click", () => {
+            const arrowEl = item.querySelector(".arrow");
+            const subMenu = item.querySelector(".sub-menu-mob");
+            if (arrowEl === null || subMenu === null) {
+                return;
+            }
+            if (item.classList.contains("open")) {
+                item.classList.remove("open");
+                subMenu.classList.remove("activeSubMenu");
+                arrowEl.style.transform = "rotate(360deg)";
+                return;
+            }
+            item.classList.add("open");
+            subMenu.classList.add("activeSubMenu");
+            arrowEl.style.transform = "rotate(180deg)";
+        });
+    });
 });
